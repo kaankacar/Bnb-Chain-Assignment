bd1d1ead1e3f5e9d5c961bce0f75c684f38c0cd1
kaankacar <kaankacar02@gmail.com>
2023-04-13 01:00:06 +0300

    Added basic arithmetic to the Counter contract

---
+++ contracts/Counter.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract Counter {
+    uint public count;
+    int public addedNum = 0;
+    int public dividedNum = 0;
+    int public subtractedNum = 0;
+    int public multipliedNum = 0;
+    function getCount() public view returns(uint){
+        return count;
+    }
+    function increment() public returns(uint){
+        count++;
+        return count;
+    }
+    function add(int _addedNum) public returns(int){
+        addedNum = int(count) + _addedNum;
+        return addedNum;
+    }
+    function multiply(int _multipliedNum) public returns(int){
+        multipliedNum = int(count) * _multipliedNum;
+        return multipliedNum;
+    }
+    function subtract(int _subtractedNum) public returns(int){
+        subtractedNum = int(count) - _subtractedNum;
+        return subtractedNum;
+    }
+    function divide(int _dividedNum) public returns(int){
+        dividedNum = int(count) / _dividedNum;
+        return dividedNum;
+    }
+    // function getAllNums() public returns(int[] memory)
+}
